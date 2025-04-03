22659584b612fa399e1458e0130b2e887511d192
Mykhajlo <bmve@ukr.net>
2025-04-03 19:30:59 +0300

    add section

---
+++ src/js/about-me.js
@@ -0,0 +1,46 @@
+import Accordion from 'accordion-js';
+import "accordion-js/dist/accordion.min.css";
+//import Swiper from 'swiper';
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/css';
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Accordion(".accordion-container", {
+    duration: 400,
+    showMultiple: false,
+    openOnInit: [0],
+  });
+
+    const swiper = new Swiper('.swiper', {
+    modules: [Navigation],
+    // Default parameters
+    slidesPerView: 2,
+        spaceBetween: 0,
+    autoplay: {
+   delay: 5000,},
+    centeredSlides: false,
+    slideToClickedSlide: true,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 768
+      768: {
+        slidesPerView: 3
+      
+      },
+      // when window width is >= 1440px
+      1440: {
+        slidesPerView: 6
+      }
+    },
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next"
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    grabCursor: true,
+  });
+});
+console.log(swiper);
