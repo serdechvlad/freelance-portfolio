fe38babf4b77a16dbf9a4ce4b6419e0b2e4d9a39
Valerii Dzhurenko-Kiorkiian <valerakiorkian@gmail.com>
2025-04-05 16:18:54 -0400

    fixed bodyscrolllock

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -29,6 +29,6 @@ import { enableBodyScroll, disableBodyScroll } from 'body-scroll-lock';
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    enableBodyScroll(document.body);
   });
 })();
