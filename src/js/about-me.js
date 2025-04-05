7f90bd2fd4f4cf6790b79993148cd574a74e260b
Mykhajlo-Berezhnyj <bmve@ukr.net>
2025-04-06 00:07:05 +0300

    add fix

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,46 +1,45 @@
 import Accordion from 'accordion-js';
-import "accordion-js/dist/accordion.min.css";
-//import Swiper from 'swiper';
+import 'accordion-js/dist/accordion.min.css';
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 //import 'swiper/css/navigation';
 
-document.addEventListener("DOMContentLoaded", () => {
-  new Accordion(".accordion-container", {
-    duration: 400,
+document.addEventListener('DOMContentLoaded', () => {
+  new Accordion('#accordion-about', {
+    duration: 300,
     showMultiple: false,
     openOnInit: [0],
   });
 
-    const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper('#swiper-about', {
     modules: [Navigation],
     // Default parameters
     slidesPerView: 2,
-        spaceBetween: 0,
+    spaceBetween: 0,
     slideToClickedSlide: true,
     // Responsive breakpoints
     breakpoints: {
-      // when window width is >= 768
       768: {
-        slidesPerView: 3
-      
+        slidesPerView: 3,
       },
-      // when window width is >= 1440px
       1440: {
-        slidesPerView: 6
-      }
+        slidesPerView: 6,
+      },
     },
     loop: true,
     navigation: {
-      nextEl: ".swiper-button-next"
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
     keyboard: {
       enabled: true,
       onlyInViewport: true,
-      },
+      pageUpDown: true,
+    },
     autoplay: {
-   delay: 5000,},
+      delay: 5000,
+    },
     grabCursor: true,
   });
 });
