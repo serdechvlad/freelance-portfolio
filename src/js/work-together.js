7b2644eaed5896f8d8dc85e0ae5ad7f10a3ac3ec
AllaP47 <allapaschinska1@gmail.com>
2025-04-06 19:30:33 +0300

    add message

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -1,13 +1,65 @@
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('contact-form');
   const emailInput = document.getElementById('email');
+  const emailWrapper = document.querySelector('.email-wrapper');
+  const emailMessage = document.querySelector('.email-message');
   const commentsInput = document.getElementById('comments');
+  const commentsWrapper = document.querySelector('.comments-wrapper');
+const commentsMessage = document.querySelector('.comments-message');
   const messageElement = document.getElementById('message');
   const modal = document.getElementById('modal');
   const backdrop = document.querySelector('.backdrop');
   const closeModalBtn = document.querySelector('.close-modal');
   const spinner = document.getElementById('spinner');
 
+
+
+  emailInput.addEventListener('input', () => {
+  const value = emailInput.value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (value === '') {
+    emailWrapper.classList.remove('success', 'error');
+    emailMessage.textContent = '';
+    emailMessage.style.display = 'none';
+    return;
+  }
+
+  if (emailRegex.test(value)) {
+    emailWrapper.classList.add('success');
+    emailWrapper.classList.remove('error');
+    emailMessage.textContent = 'Success!';
+    emailMessage.style.display = 'block';
+  } else {
+    emailWrapper.classList.add('error');
+    emailWrapper.classList.remove('success');
+    emailMessage.textContent = 'Invalid email, try again';
+    emailMessage.style.display = 'block';
+  }
+});
+  
+  commentsTextarea.addEventListener('input', () => {
+  const value = commentsTextarea.value.trim();
+
+  if (value === '') {
+    commentsWrapper.classList.remove('success', 'error');
+    commentsMessage.textContent = '';
+    commentsMessage.style.display = 'none';
+    return;
+  }
+
+  if (value.length >= 10) {
+    commentsWrapper.classList.add('success');
+    commentsWrapper.classList.remove('error');
+    commentsMessage.textContent = 'Success!';
+    commentsMessage.style.display = 'block';
+  } else {
+    commentsWrapper.classList.add('error');
+    commentsWrapper.classList.remove('success');
+    commentsMessage.textContent = 'Please enter at least 10 characters';
+    commentsMessage.style.display = 'block';
+  }
+});
   // Функція для валідації email
   function validateEmail() {
     const emailValue = emailInput.value;
